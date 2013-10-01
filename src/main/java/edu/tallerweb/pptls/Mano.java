aef1082e61783ffe22cd5c215ca26d5edc2439d7
Monti <monto_rober@yahoo.com>
2013-10-01 16:30:27 -0300

    Se modifica el estilo del igual de la clase Mano

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -13,7 +13,7 @@ public class Mano {
     private Forma forma;
 	
 	public Mano(final Forma forma) {
-		 this.forma=forma;
+		 this.forma = forma;
 	}
 
 	/**
@@ -25,10 +25,10 @@ public class Mano {
 	
 	public Resultado jugarCon(final Mano otra) {
 		
-		Resultado matriz[][]=new Resultado[5][5];
-		matriz[0][0]=matriz[1][1]=matriz[2][2]=matriz[3][3]=matriz[4][4]=Resultado.EMPATA;
-		matriz[0][3]=matriz[0][4]=matriz[1][0]=matriz[1][4]=matriz[2][0]=matriz[2][1]=matriz[3][1]=matriz[3][2]=matriz[4][2]=matriz[4][3]=Resultado.GANA;
-		matriz[0][1]=matriz[0][2]=matriz[1][2]=matriz[1][3]=matriz[2][3]=matriz[2][4]=matriz[3][0]=matriz[3][4]=matriz[4][0]=matriz[4][1]=Resultado.PIERDE;
+		Resultado matriz[][] = new Resultado[5][5];
+		matriz[0][0] = matriz[1][1] = matriz[2][2] = matriz[3][3] = matriz[4][4] = Resultado.EMPATA;
+		matriz[0][3] = matriz[0][4] = matriz[1][0] = matriz[1][4] = matriz[2][0] = matriz[2][1] = matriz[3][1] = matriz[3][2] = matriz[4][2] = matriz[4][3] = Resultado.GANA;
+		matriz[0][1] = matriz[0][2] = matriz[1][2] = matriz[1][3] = matriz[2][3] = matriz[2][4] = matriz[3][0] = matriz[3][4] = matriz[4][0] = matriz[4][1] = Resultado.PIERDE;
 		
 		return matriz[this.forma.getValor()][otra.forma.getValor()]; 
 	}
