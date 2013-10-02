7a8218b97b5ebee7a33ede3c8edba46275f8ef6a
Monti <monto_rober@yahoo.com>
2013-10-02 19:45:49 -0300

    Se modifica el estilo del metodo Jugar con de la Clase mano

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -10,8 +10,8 @@ public class Mano {
 	 * la que determine su condición en el juego.
 	 * @param forma, la Forma que adopta la Mano.
 	 */
-    private Forma forma;
-	
+    private final Forma forma;
+
 	public Mano(final Forma forma) {
 		 this.forma = forma;
 	}
@@ -22,15 +22,18 @@ public class Mano {
 	 * @param otra, la otra Mano.
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
-	
+
 	public Resultado jugarCon(final Mano otra) {
-		
-		Resultado matriz[][] = new Resultado[5][5];
-		matriz[0][0] = matriz[1][1] = matriz[2][2] = matriz[3][3] = matriz[4][4] = Resultado.EMPATA;
-		matriz[0][3] = matriz[0][4] = matriz[1][0] = matriz[1][4] = matriz[2][0] = matriz[2][1] = matriz[3][1] = matriz[3][2] = matriz[4][2] = matriz[4][3] = Resultado.GANA;
-		matriz[0][1] = matriz[0][2] = matriz[1][2] = matriz[1][3] = matriz[2][3] = matriz[2][4] = matriz[3][0] = matriz[3][4] = matriz[4][0] = matriz[4][1] = Resultado.PIERDE;
-		
-		return matriz[this.forma.getValor()][otra.forma.getValor()]; 
+
+		int CERO=0,UNO=1,DOS=2,TRES=3,CUATRO=4,CINCO=5;
+        Resultado[][] matriz = new Resultado[CINCO][CINCO] ;
+
+        matriz[CERO][CERO] = matriz[UNO][UNO] = matriz[DOS][DOS] = matriz[TRES][TRES] = matriz[CUATRO][CUATRO] = Resultado.EMPATA;
+		matriz[CERO][TRES] = matriz[CERO][CUATRO] = matriz[UNO][CERO] = matriz[UNO][CUATRO] = matriz[DOS][CERO] = matriz[DOS][UNO] = matriz[TRES][UNO] = matriz[TRES][DOS] = matriz[CUATRO][DOS] = matriz[CUATRO][TRES] = Resultado.GANA;
+		matriz[CERO][UNO] = matriz[CERO][DOS] = matriz[UNO][DOS] = matriz[UNO][TRES] = matriz[DOS][TRES] = matriz[DOS][CUATRO] = matriz[TRES][CERO] = matriz[TRES][CUATRO] = matriz[CUATRO][CERO] = matriz[CUATRO][UNO] = Resultado.PIERDE;
+		Resultado formaseleccionada=matriz[this.forma.getValor()][otra.forma.getValor()];
+
+		return formaseleccionada;
 	}
 
 }
