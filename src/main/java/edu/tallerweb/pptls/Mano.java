70f242fde4f9dbb7e37ec0a58ccb38e4525841f2
Monti <monto_rober@yahoo.com>
2013-10-02 20:14:21 -0300

    Nueva modificacion del estilo del metodo Jugar con

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -24,14 +24,14 @@ public class Mano {
 	 */
 
 	public Resultado jugarCon(final Mano otra) {
-        Resultado[][] matriz = new Resultado[5][5] ;
 
-        matriz[0][0] = matriz[1][1] = matriz[2][2] = matriz[3][3] = matriz[4][4] = Resultado.EMPATA;
+		Resultado[][]matriz = new Resultado[5][5] ;
+		matriz[0][0] = matriz[1][1] = matriz[2][2] = matriz[3][3] = matriz[4][4] = Resultado.EMPATA;
 		matriz[0][3] = matriz[0][4] = matriz[1][0] = matriz[1][4] = matriz[2][0] = matriz[2][1] = matriz[3][1] = matriz[3][2] = matriz[4][2] = matriz[4][3] = Resultado.GANA;
 		matriz[0][1] = matriz[0][2] = matriz[1][2] = matriz[1][3] = matriz[2][3] = matriz[2][4] = matriz[3][0] = matriz[3][4] = matriz[4][0] = matriz[4][1] = Resultado.PIERDE;
-		Resultado formaseleccionada=matriz[this.forma.getValor()][otra.forma.getValor()];
 
-		return formaseleccionada;
+	    Resultado formaseleccionada=matriz[this.forma.getValor()][otra.forma.getValor()];
+	    return formaseleccionada;
 	}
 
 }
